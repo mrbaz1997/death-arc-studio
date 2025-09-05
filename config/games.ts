a9c0322f12b9fc01f9ef534d88b173c363c21671
Îdrîs <33225175+mrbaz1997@users.noreply.github.com>
2025-09-05 12:37:48 +0330

    Update games.ts

--- config/games.ts
+++ config/games.ts
@@ -7,27 +7,51 @@ export interface Game {
 
 export const games: Game[] = [
   {
-    title: "Mystic Realms",
-    description: "An epic fantasy RPG with stunning visuals and immersive storytelling.",
-    image: "/placeholder-bs772.png",
+    title: "Spacy",
+    description: "Shoot'em-Up Arcade Game.",
+    image: "https://drive.google.com/file/d/1dNGVhVdiElSQOfZurWf0MWk83bXCMJFY/view?usp=drive_link",
+    status: "Coming Soon",
+  },
+  {
+    title: "Tic Tac Toe 2",
+    description: "3D tic tac toe board game with gobblet gabbler's rules.",
+    image: "https://drive.google.com/file/d/1jMeOWJYxdG4J87CTQt9_5wzI-czkH8lM/view?usp=sharing",
     status: "Released",
   },
   {
-    title: "Cyber Runner",
-    description: "Fast-paced cyberpunk action game set in a dystopian future.",
-    image: "/placeholder-i5omk.png",
-    status: "In Development",
-  },
-  {
-    title: "Puzzle Nexus",
-    description: "Mind-bending puzzle game that challenges your problem-solving skills.",
-    image: "/colorful-puzzle-game-interface.jpg",
+    title: "Lastbit",
+    description: "Ultimate survival challenge strategy game.",
+    image: "https://drive.google.com/file/d/13uws4zEOBi0EMp-09NCFZherTmvhar9n/view?usp=sharingg",
     status: "Released",
   },
   {
-    title: "Space Odyssey",
-    description: "Explore the cosmos in this ambitious space exploration adventure.",
-    image: "/placeholder-xp1e4.png",
+    title: "Snake Tower",
+    description: "Swipe, grow, and blast enemies in this epic snake-shooter roguelike!",
+    image: "https://drive.google.com/file/d/1ULLDz_KvFV_tap7uxhdSFiZNlEfE4__1/view?usp=sharing",
+    status: "Coming Soon",
+  },
+  {
+    title: "Rushtaurant",
+    description: "A Merging Restaurant game.",
+    image: "https://drive.google.com/file/d/1EmqrAyTV9cmH_GJq4Ob6j9x4lpdocpQD/view?usp=sharing",
+    status: "Coming Soon",
+  },
+  {
+    title: "Spinning Tank",
+    description: "A fast-paced one-tap arcade game where your player loops endlessly around the track, and a single tap reverses your direction to dodge monsters and survive.",
+    image: "https://drive.google.com/file/d/1VxHyN0AX5WocVupMeywvo6XU9MColkUF/view?usp=drivesdk",
+    status: "Coming Soon",
+  },
+  {
+    title: "BlackSmith",
+    description: "A mid-core Tower defense game (includes rougelike, backpack and merging elements), a Blacksmith that should make weapons as much as he can for defense.",
+    image: "https://drive.google.com/file/d/1UsRfqaHcn7Xk-d0goOPEiFdwdEX-kLMD/view?usp=sharing",
+    status: "Coming Soon",
+  },
+  {
+    title: "The Rolling Ball",
+    description: "A 3d swiping Ball game, with good level design and physic challenges.",
+    image: "https://drive.google.com/file/d/1jkOkGQwJnyaeOin82zSXfOc3dmbnRQf7/view?usp=sharing",
     status: "Coming Soon",
   },
 ]
