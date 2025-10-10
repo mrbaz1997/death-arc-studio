aedfbb05af2f9d00011f8cedbe00078ec788e5b1
Îdrîs <33225175+mrbaz1997@users.noreply.github.com>
2025-10-10 12:00:52 +0330

    Restore header

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -12,14 +12,6 @@ export function HeroSection() {
           Death Arc Studio is a passionate team of game developers creating innovative, engaging games that push the
           boundaries of interactive entertainment.
         </p>
-        <div className="flex flex-col sm:flex-row gap-4 justify-center">
-          <Button size="lg" className="text-lg px-8">
-            View Our Games
-          </Button>
-          <Button variant="outline" size="lg" className="text-lg px-8 bg-transparent">
-            Learn More
-          </Button>
-        </div>
       </div>
     </section>
   )
